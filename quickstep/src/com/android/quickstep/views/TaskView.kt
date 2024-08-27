141a4c9857422aaa9dc12a85385d6bd136e29e10
Alex Chau <alexchau@google.com>
2024-08-27 17:37:55 +0100

    Convert DigitalWellBeingToast to Kotlin
    Bug: DigitalWellBeingToast
    Test: manual
    Flag: EXEMPT refactor
    Change-Id: I9998269b32e89725e311c3ca88e924a55c196412

--- quickstep/src/com/android/quickstep/views/TaskView.kt
+++ quickstep/src/com/android/quickstep/views/TaskView.kt
@@ -1398,7 +1398,7 @@ constructor(
     private fun onFocusTransitionProgressUpdated(focusTransitionProgress: Float) {
         taskContainers.forEach {
             it.iconView.setContentAlpha(focusTransitionProgress)
-            it.digitalWellBeingToast?.updateBannerOffset(1f - focusTransitionProgress)
+            it.digitalWellBeingToast?.bannerOffsetPercentage = 1f - focusTransitionProgress
         }
     }
 
@@ -1548,7 +1548,7 @@ constructor(
     private fun onModalnessUpdated(modalness: Float) {
         taskContainers.forEach {
             it.iconView.setModalAlpha(1 - modalness)
-            it.digitalWellBeingToast?.updateBannerOffset(modalness)
+            it.digitalWellBeingToast?.bannerOffsetPercentage = modalness
         }
     }
 
